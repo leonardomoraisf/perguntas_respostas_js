693d5d5850724a92f76db54ec397909d53b40078
Leonardo Morais Franca <mfrancaleonardo@gmail.com>
2022-12-07 14:11:14 -0300

    Lógica completa

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ var elementos = document.querySelectorAll('[type=radio]');
 
 for(var i = 0; i < elementos.length; i++){
     elementos[i].addEventListener('change', (e)=>{
+
         let marcado = e.target.value;
+
         if(marcado == "correta"){
-            let el = e.target.previousElementSibling.style.backgroundColor = "green";
+
+            let parentNode = e.target.parentNode;
+            parentNode.style.backgroundColor = 'green';
+            parentNode.style.borderRadius = '0.375rem';
+
+            let els = parentNode.parentNode.querySelectorAll('[type=radio]');
+
+            for(var n = 0; n < els.length; n++){
+                els[n].disabled = true;
+            }
+
         }else if(marcado == "incorreta"){
-            let el = e.target.previousElementSibling.style.backgroundColor = "red";
+
+            let parentNode = e.target.parentNode;
+            parentNode.style.backgroundColor = 'red';
+            parentNode.style.borderRadius = '0.375rem';
+
+            let els = parentNode.parentNode.querySelectorAll('[type=radio]');
+
+            for(var n = 0; n < els.length; n++){
+                els[n].disabled = true;
+            }
+
+            let correta = parentNode.parentNode.querySelector('[value=correta]');
+            correta.parentNode.style.backgroundColor = 'green';
+            correta.parentNode.style.borderRadius = '0.375rem';
+
         }
     })
 }
